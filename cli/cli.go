248092851317ef9b7445e87eb1f65d965b066c0b
iou4 <ouchaibimad000@gmail.com>
2024-10-28 16:53:49 +0100

    good progress on file tests

--- cli/cli.go
+++ cli/cli.go
@@ -1,16 +1,26 @@
 package cli
 
 import (
+	"log"
 	"strconv"
 
 	"ou.emad/core"
 )
 
-func Run(url string, status int, body, file string) error {
+func Run(url string, status int, body, file string) {
+	if file != "" {
+		results, err := core.RunFileTest(file)
+		if err != nil {
+			log.Fatal(err)
+		}
+		for _, v := range results {
+			v.Print()
+		}
+		return
+	}
 	result, err := core.RunTest(core.Request{Url: url, Status: strconv.Itoa(status), Body: body})
 	if err != nil {
-		return err
+		log.Fatal(err)
 	}
 	result.Print()
-	return nil
 }
--- core/main.go
+++ core/main.go
@@ -1,9 +1,11 @@
 package core
 
 import (
+	"encoding/json"
 	"io"
 	"log"
 	"net/http"
+	"os"
 	"strings"
 	"time"
 )
@@ -23,9 +25,7 @@ func RunTest(request Request) (*Result, error) {
 	if err != nil {
 		return nil, err
 	}
-	log.Printf("after %vms, got reponse status: %v and response content: %v\n",
-		time.Now().Sub(took).Milliseconds(),
-		response.Status, response.Header.Get("Content-Type"))
+	log.Printf("test done after %vms.\n", time.Now().Sub(took).Milliseconds())
 	bodyMatch := testBody(body, test.Body)
 	statusMatch := getStatusNumber(response.Status) == test.Status
 	result := &Result{BodyMatch: bodyMatch, StatusMatch: statusMatch}
@@ -38,4 +38,38 @@ func getStatusNumber(rawStatus string) string {
 
 func testBody(respBody []byte, expectedBody string) bool {
 	return strings.TrimSpace(string(respBody)) == strings.TrimSpace(expectedBody)
+}
+
+func RunFileTest(path string) ([]*Result, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+	var requests []Request
+	var data []byte
+	buf := make([]byte, 100)
+	for {
+		n, err := file.Read(buf)
+		data = append(data, buf[0:n]...)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			return []*Result{}, err
+		}
+	}
+	err = json.Unmarshal(data, &requests)
+	if err != nil {
+		return []*Result{}, err
+	}
+	var results []*Result
+	for _, request := range requests {
+		result, err := RunTest(request)
+		if err != nil {
+			return []*Result{}, err
+		}
+		results = append(results, result)
+	}
+	return results, nil
 }
